c7c3ae17854e72062a1cef29cb1d1cfe70df3fb2
Vadim <belay.vadim.95@gmail.com>
2022-08-29 20:45:26 +0300

    0-29-08-22

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from top_secret import MY_SECRET_KEY
-
+ 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
